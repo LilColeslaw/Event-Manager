6cd5fc03cc7cb610522beaf2d4968dea60576509
Winslow Solomon <winsolo11@gmail.com>
2022-08-26 09:55:54 -0400

    Fix problem with API and continue working
    The api is different now than it was when the tutorial was first made,
    so I ran into some problems with it. Using help from people on discord,
    I fixed it. This commit now can find and print out the legislators of
    each person based on their zip-code.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,9 +1,38 @@
 require 'csv'
-puts 'Event Manager Initialized!'
+require 'google/apis/civicinfo_v2'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  stdout = $stdout
+  stderr = $stderr
+  begin
+    $stdout = $stderr = StringIO.new
+    legislators = civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    )
+    $stdout = stdout
+    $stderr = stderr
+    legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
+
+    legislators_string = legislator_names.join(', ')
+  rescue
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  ensure
+    $stdout = stdout
+    $stderr = stderr
+  end
+end
+
+puts 'EventManager initialized.'
 
 contents = CSV.open(
   'event_attendees.csv',
@@ -12,7 +41,11 @@ contents = CSV.open(
 )
 
 contents.each do |row|
-  first_name = row[:first_name]
+  name = row[:first_name]
+
   zipcode = clean_zipcode(row[:zipcode])
-  p "#{first_name} - #{zipcode}"
+
+  legislators = legislators_by_zipcode(zipcode)
+
+  puts "#{name} #{zipcode} #{legislators}"
 end
